2ed1b1e9bb6b473c8bd59a222c2a6a6bdc591e42
clarice <142429120+clarisv@users.noreply.github.com>
2023-09-01 10:51:37 -0300

    Update operadores2.c

--- operadores2.c
+++ operadores2.c
@@ -1,33 +1,31 @@
-#include <stdio.h>
+#include<stdio.h>
 
-main() {
+main(){
 
-  int valorA, valorB, valorC;
-    
-  printf("Digite três valores: ");
-  scanf("%d %d %d", &valorA, &valorB, &valorC);
-    
-    // Ordenação utilizando a lógica de trocas
+  //Construa um algoritmo que, dado quatro valores, A, B, C e D, o algoritmo imprima o maior e o menor valor. 
 
-    if (valorA < valorB) {
-        int temp = valorA;
-        valorA = valorB;
-        valorB = temp;
-    }
-    
-    if (valorB < valorC) {
-        int temp = valorB;
-        valorB = valorC;
-        valorC = temp;
-    }
-    
-    if (valorA < valorB) {
-        int temp = valorA;
-        valorA = valorB;
-        valorB = temp;
-    }
-    
-    printf("Valores em ordem decrescente: %d %d %d\n", valorA, valorB, valorC);
-    
+  int A = 10, B = 8, C = 6, D = 4, maior, menor;
   
+  //verificar o maior de todos
+  if(A > B && A > C && A > D){
+     maior = A;
+  }else if(B > A && B > C && B > D){
+     maior = B;
+  }else if(C > B && C > A && C > D){
+     maior = C;
+  }else{
+     maior = D;
+  }
+   
+  //verificar o menor de todos
+  if(A < B && A < C && A < D){
+     menor = A;
+  }else if(B < A && B < C && B < D){
+     menor = B;
+  }else if(C < B && C < A && C < D){
+     menor = C;
+  }else{
+     menor = D;
+  }
+  printf("maior: %d,. menor: %d", maior, menor);
 }
